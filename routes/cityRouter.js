b7707429d53e30dee5053305b73ea834b5c5d885
Asrath <pasrath.2004@gmail.com>
2025-03-18 10:55:44 +0530

    added comments

--- routes/cityRouter.js
+++ routes/cityRouter.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const cityController = require('../controllers/admin/cityController');
 const { protect, restrictTo } = require('../middlewares/authmiddlewares');
 
-router.use(protect);
-router.use(restrictTo(2));
+router.use(protect); // ensures that user is logged in
+router.use(restrictTo(2)); // ensures that only user_type = 2 can access resources
 
 router.route('/').get(cityController.getCities).post(cityController.createCity);
 router
