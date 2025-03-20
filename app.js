dd8a5b23f469b4688f444bb13a572b4388efd3cc
Asrath <pasrath.2004@gmail.com>
2025-03-20 14:25:07 +0530

    added multer

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Static files setup
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
