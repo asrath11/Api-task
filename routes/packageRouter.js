49df41ef9f469ff0e14cbebdb294bcf8bbdfd4cb
Asrath <pasrath.2004@gmail.com>
2025-03-20 14:23:19 +0530

    added multer middleware

--- routes/packageRouter.js
+++ routes/packageRouter.js
@@ -7,13 +7,13 @@ const { protect, restrictTo } = require('../middlewares/authmiddlewares');
 
 router.use(protect); // ensures that user is logged in
 router.use(restrictTo(2)); // ensures that only user_type = 2 can access resources
-router
-  .route('/')
-  .get(packageController.getPackages)
-  .post(
-    upload('public/img/packages', 'packages'),
-    packageController.createPackage
-  );
+router.route('/').get(packageController.getPackages);
+
+router.post(
+  '/test-upload',
+  upload('public/img/packages', 'packages'),
+  packageController.createPackage
+);
 
 router
   .route('/:id')
